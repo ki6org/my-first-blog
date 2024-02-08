3aab7baa73bdddd09a87032383a964179bc14a28
ki6org <gleb.ki6org@gmail.com>
2024-02-08 10:46:55 +0600

    08.02

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    ('bootstrap', 'ki6org.pythonanywhere.com/static/css/),
+    ('bootstrap', 'ki6org.pythonanywhere.com/static/css/'),
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
